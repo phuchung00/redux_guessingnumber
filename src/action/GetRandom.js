ac65834b5e8a828b8178efe66213a2d7da4ce4ff
phuchung <nphbo1998@gmail.com>
2019-05-10 09:39:36 +0700

    done

---
+++ src/action/GetRandom.js
@@ -0,0 +1,10 @@
+const RANDOM_NUMBER = 'action/RANDOM'
+
+export const getRandomAction = () => {
+  return {
+    type:RANDOM_NUMBER,
+  }
+}
+export default function randomReducer (state , action){
+  
+}
---
+++ src/component/Guessing.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Guessing extends Component {
+  
+  render() {
+    return (
+      <div>
+        Guessing Number
+        <div>
+          <button>1</button>
+        </div>
+        <div>
+          <button>2</button>
+        </div>
+        <div>
+          <button>3</button>
+        </div>
+      </div>
+    )
+  }
+}
